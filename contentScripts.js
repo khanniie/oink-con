42f948fb1a277ebf6a67bf99b8c1ca26f2ad04a6
Constance Ye <constanceye1316@gmail.com>
2019-02-09 16:32:40 -0500

    new stuff

--- contentScripts.js
+++ contentScripts.js
@@ -1,8 +1,70 @@
-farnamImageUrls = [
-	"https://www.cs.cmu.edu/~farnam/images/Farnam-Jahanian.jpg",
-	"http://web.eecs.umich.edu/~farnam/images/fj.jpg",
-	"https://www.cmu.edu/news/stories/archives/2015/february/images/farnam-jahanian-400x250-min.jpg"
-]
+// chrome.runtime.onMessage.addListener(gotMessage);
+
+// function gotMessage(message, sender, sendResponse) {
+//     console.log(message);
+var message = {txt: "on"};
+    if (message.txt === "on") {
+        let paragraphs = document.getElementsByTagName('p');
+        let lists = document.getElementsByTagName('li');
+        let links = document.getElementsByTagName('a');
+        let h1s = document.getElementsByTagName('h1');
+        let h2s = document.getElementsByTagName('h2');
+        let h3s = document.getElementsByTagName('h3');
+        let h4s = document.getElementsByTagName('h4');
+
+        for (elt of paragraphs) {
+            elt.style['font-size'] = '28px';
+            elt.style['line-height'] = '1.6';
+            elt.style['letter-spacing'] = '0.2px';
+        }
+        for (elt of lists) {
+            elt.style['font-size'] = '27px';
+            elt.style['line-height'] = '1.4';
+        }
+        for (elt of links) {
+            elt.style['font-size'] = '27px';
+            elt.style['line-height'] = '1.4';
+            elt.style['font-weight'] = '600';
+            elt.style['color'] = '#1c63b2';
+        }
+        for (elt of h1s) {
+            elt.style['font-size'] = '36px';
+            elt.style['line-height'] = '1.4';
+            elt.style['font-weight'] = '600';
+            elt.style['font-family'] = 'sans-serif';
+            elt.style['letter-spacing'] = '0.3px';
+            elt.style['text-transform'] = 'uppercase';
+        }
+        for (elt of h2s) {
+            elt.style['font-size'] = '35px';
+            elt.style['line-height'] = '1.4';
+            elt.style['font-weight'] = '600';
+            elt.style['font-family'] = 'sans-serif';
+            elt.style['letter-spacing'] = '0.3px';
+            elt.style['text-transform'] = 'uppercase';
+        }
+        for (elt of h3s) {
+            elt.style['font-size'] = '35px';
+            elt.style['line-height'] = '1.4';
+            elt.style['font-weight'] = '600';
+            elt.style['font-family'] = 'sans-serif';
+            elt.style['letter-spacing'] = '0.3px';
+            elt.style['text-transform'] = 'uppercase';
+            elt.style['color'] = 'gray';
+        }
+        for (elt of h4s) {
+            elt.style['font-size'] = '32px';
+            elt.style['line-height'] = '1.4';
+            elt.style['font-weight'] = '600';
+            elt.style['font-family'] = 'sans-serif';
+            elt.style['letter-spacing'] = '0.3px';
+            elt.style['text-transform'] = 'uppercase';
+            elt.style['color'] = 'gray';
+        }
+    }
+// }
+
+
 
 var mySocket = io("http://198.199.65.72:4000/");
     mySocket.on('connect', function(){console.log("connected")});
@@ -11,32 +73,75 @@ var mySocket = io("http://198.199.65.72:4000/");
     mySocket.on('labels', function(data){
         console.log('recieved response', data);
         var idx = data.class;
-        var labels = data.labels.slice(0, 5).map(x => x.description);
-        if(labels.length == 0) return;
-        var ele = taglessimages[idx];
-        var str = labels.reduce((acc, curr) => acc + ", " + curr);
-        if (ele) {ele.alt = str} else{
-            console.log(taglessimages, ele);
+        //TODO: set this based on toggle
+      filterarr=["spider","bear","motorcycle"];
+        var filter = true;
+        if (filter){
+          var done = false;
+          console.log(filterarr)
+          for (var f = 0; f < filterarr.length; f++){
+            for (var l=0; l < data.labels.length; l++){
+              console.log(filterarr[f]);
+              var labelstr = (data.labels[l].description).toLowerCase()+"";
+              console.log(labelstr)
+              console.log(labelstr.indexOf(filterarr[f]))
+              if (labelstr.indexOf(filterarr[f]) >= 0){
+                images[idx].src=chrome.runtime.getURL('pig.png');
+                images[idx].alt = "piggy";
+                done = true;
+                break;
+              }
+            }
+            if (done) break;
+          }
         }
+        var alttags = true;
+      if (alttags){
+        if (taglessimages.includes(images[idx])){
+          console.log
+          var labels = data.labels.slice(0, 5).map(x => x.description);
+          if(labels.length == 0) return;
+          var ele = images[idx];
+          var str = labels.reduce((acc, curr) => acc + ", " + curr);
+          if (ele) {ele.alt = str} else{
+              console.log(images, ele);
+          }
+        }
+      }
     });
 
 var taglessimages = [];
-var index = 0;
+var images = []
+//var index = 0;
+  //get array from storage of trigger words
+  var filterarr = ["spider","bear","motorcycle"];
 
 function execute(){
     mySocket.emit('connietest');
     chrome.storage.local.get('updateTextTo', function(items){
         console.log(items);
     });
-    var images = document.getElementsByTagName("img"); //this is an array of images
+    
+    //get tags
+    chrome.storage.local.get('tags',function(items){
+      filterarr = items;
+    });
+    
+
+
+  
+  
+    images = document.getElementsByTagName("img"); //this is an array of images
     for(var i = 0; i < images.length; i++){
         if(images[i].alt == ""){
                 taglessimages.push(images[i]);
-                mySocket.emit('labelimage', {url: images[i].src, class: index});
-                index++;
         };
+      mySocket.emit('labelimage', {url: images[i].src, class: i});
     }
     return images.length;
+  
+  
+  
 }
 
 var changed = execute();
--- options.js
+++ options.js
@@ -1,4 +1,19 @@
-    document.getElementById("addblocktag").addEventListener("click", function(){
+function setupImage(){
+    chrome.storage.local.get('tags', function(items){
+            var listof = items.tags;
+            var reduced = listof.reduce((arr, c) => arr + " " + c);
+            document.getElementById("blockedimagelist").innerHTML = "blocked tag list: " + reduced;
+        });
+    chrome.storage.local.get('url', function(items){
+            var u = items.url;
+            if(u && u != ""){
+                document.getElementById("appliedurl").innerHTML = "current applied url:: " + u;
+            }
+            
+        });
+}
+
+document.getElementById("addblocktag").addEventListener("click", function(){
         console.log("clicked");
         var str = document.getElementById("blockedimagelist").innerHTML;
         var value = document.getElementById("texttag").value;
@@ -6,5 +21,30 @@
         str = str + " " + value;
         document.getElementById("blockedimagelist").innerHTML = str;
         console.log(str);
+        chrome.storage.local.get('tags', function(items){
+            console.log(items.tags);
+            var newitems = [];
+            newitems = items.tags;
+            newitems.push(value);
+            chrome.storage.local.set({
+            tags: newitems
+            });
+        });
+        
         return false;
     })
+
+document.getElementById("addurl").addEventListener("click", function(){
+        console.log("clicked url");
+        var value = document.getElementById("urltag").value;
+        if(!value || value === "") return;
+        var str = "current applied url: "  + value;
+        document.getElementById("appliedurl").innerHTML = str;
+        chrome.storage.local.set({
+            url: value
+        });
+        
+        return false;
+    })
+
+setupImage();
